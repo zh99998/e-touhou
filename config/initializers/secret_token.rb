3e69e85aa37ace349af4ce9a66ea1da90fa2b9d1
zh99998 <zh99998@gmail.com>
2013-03-12 16:30:49 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ETouhou::Application.config.secret_token = '1cdbb1f74bb8fc9bd30459d997b9dbbb34a70b4f4940dfcc29181b46729710d72427500e163e2acf71c92519932ad4ea43b345571a517994bb10ff3ebde902bc'
