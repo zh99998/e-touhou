09d629e91cf81bdc9c607e9d2e413859868b1b3c
zh99998 <zh99998@gmail.com>
2013-04-08 21:44:01 +0800

    size

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,8 +5,8 @@ class Page < ActiveRecord::Base
   # attr_accessible :title, :body
   has_attached_file :attachment,
                     :styles => {
-                        :thumb => "75x100",
-                        :small => "150x200",
+                        :thumb => "75x100>",
+                        :small => "150x200>",
                         :large => "1000x8000>"
                     }
   validates_attachment_presence :attachment
