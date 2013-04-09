0dee2ea2ddcff2fb87b4073f66d2f91da9c4ed0f
zh99998 <zh99998@gmail.com>
2013-04-09 09:23:31 +0800

    category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
   attr_accessible :name
   has_many :comics
+
+  attr_accessible :icon
+  has_attached_file :icon, :styles => { :icon => '40x50#' }, :default_url => '/images/logo_hot.png'
+
 end
